c3ac637c4140759b50781f7df7034622c74cc7b1
Cristian Silvera <cristiansilveraa@gmail.com>
2022-12-24 11:34:17 -0300

    feat: implemented of notifications mapping

--- src/infra/http.module.ts
+++ src/infra/http.module.ts
@@ -2,7 +2,6 @@ import { Module } from "@nestjs/common";
 import { SendNotification } from "src/application/use-cases/send-notification";
 import { DatabaseModule } from "./database/database.module";
 import { NotificationsController } from "./http/controllers/notifications.controller";
-// 1:26:26
 
 @Module({
     imports: [DatabaseModule],
@@ -10,4 +9,4 @@ import { NotificationsController } from "./http/controllers/notifications.contro
     providers: [SendNotification],
 })
 
-export class HttpModule {}
+export class HttpModule { }
